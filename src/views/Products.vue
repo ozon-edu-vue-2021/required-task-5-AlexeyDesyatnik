b5029a32f0855ba776179288e218f31c46ab7a98
Alexey Desyatnik <alexey.desyatnik@gmail.com>
2021-11-17 10:54:45 +0300

    Moved Product components to separate folder

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-import ProductList from '@/components/ProductList.vue';
+import ProductList from '@/components/Products/ProductList.vue';
 
 export default {
   components: {
