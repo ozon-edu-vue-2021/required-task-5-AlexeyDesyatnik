3a9c54e422689856ff21a09688c9bb122f5d4add
Alexey Desyatnik <alexey.desyatnik@gmail.com>
2021-11-17 10:40:27 +0300

    Added random product images

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="productCard">
+    <img
+      :src="require(`@/assets/images/${product.imageSrc}`)"
+      class="productImage"
+    />
     <p>Артикул: {{ product.id }}</p>
     <p>Название: {{ product.name }}</p>
     <p>Цена: {{ product.price }} руб.</p>
@@ -18,9 +22,15 @@ export default {
 <style scoped>
 .productCard {
   border: 3px solid rgb(115, 163, 172);
-  border-radius: 8px;
+  border-radius: 16px;
   margin: 16px;
   padding: 8px;
   width: 250px;
+}
+
+.productImage {
+  width: 100px;
+  height: 100px;
+  border-radius: 10px;
 }
 </style>
